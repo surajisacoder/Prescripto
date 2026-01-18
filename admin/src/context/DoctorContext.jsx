f25bd4551c0e8e9bd7153fcee26727adce9b9722
Suraj Kumar <kumarsuraj99184@gmail.com>
2026-01-18 19:59:50 +0530

    Initial commit

---
+++ admin/src/context/DoctorContext.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { createContext } from "react";
+
+export const DoctorContext = createContext();
+const DoctorContextProvider = (props) => {
+  const value = {};
+
+  return (
+    <DoctorContext.Provider value={value}>
+      {props.children}
+    </DoctorContext.Provider>
+  );
+};
+
+export default DoctorContextProvider;
---
+++ admin/src/pages/Admin/AllApointments.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllApointments = () => {
+  return <div>AllApointment</div>;
+};
+
+export default AllApointments;
